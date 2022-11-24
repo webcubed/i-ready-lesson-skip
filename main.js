4ee552b2c50d8376c563a7e81bd42d27a23e9758
wang <114349746+WangOnTop@users.noreply.github.com>
2022-11-24 10:50:12 -0500

    documented

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-function exposeHook() {
-    debug(lessonBridge.close)
-    lessonBridge.close()
+// utils
+function exposeObj(fun) {
+    debug(fun) // enter dun scope when fun called.
+    fun() //calls fun.
 }
 
-function hookObj() {
-    window.hook = _0xcc7fd3
+function hookObj(extractLocation, obj) {
+    window[extractLocation] = obj // creates global hook.
 }
+
+//part 1
+exposeObj(lessonBridge.close) //exposes the 'lessonBridge.close' fun.
+
+// part 2
+hookObj('hook', _0xcc7fd3) // extracts the '_0xcc7fd3' variable
 
 function p1(score) {
-    let csid = html5Iframe.src.split('csid=')[1].split('&type')[0]
-    objScore = {
+    let csid = html5Iframe.src.split('csid=')[1].split('&type')[0] // gets the current lesson csid
+    objScore = { // makes the score an obj
         score: score
     }
-    hook.completeLessonComponent(csid, objScore);
+    hook.completeLessonComponent(csid, objScore); // sends the request to complete the lesson
 }
