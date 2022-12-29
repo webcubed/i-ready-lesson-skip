91654237043a41ce9e8b2b3177941b0fefc6d899
Nathan <77589042+oofish8888@users.noreply.github.com>
2022-12-28 20:10:01 -0500

    keepin up wit wang

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
 //part 1
-function exposeObj(fun) {
-    debug(fun) // enter fun scope when fun called.
-    fun() //calls fun.
-}
-
-function hookObj(extractLocation, obj) {
-    window[extractLocation] = obj // creates global hook.
-}
-
-exposeObj(lessonBridge.close) //exposes the 'lessonBridge.close' fun.
+debug(lessonBridge.pause) // exposes scope when function called
+lessonBridge.pause() //calls the 'lessonBridge.pause' func.
 
 // part 2
-hookObj('hook', _0xcc7fd3) // extracts the '_0xcc7fd3' variable
-
-// part 3
-function p1(score) {
-    let csid = html5Iframe.src.split('csid=')[1].split('&type')[0] // gets the current lesson csid
-    objScore = { // makes the score an obj
-        score: score
-    }
-    hook.completeLessonComponent(csid, objScore); // sends the request to complete the lesson
+window.mainHook = _0xcc7fd3; // extracts the '_0xcc7fd3' variable to get the completeLessonComponent func.
+window.p1 = function(score) {
+     let csid = html5Iframe.src.split('csid=')[1].split('&type')[0] // gets the current lesson csid
+    objScore = { score: score } //makes the score into an obj to make server happy
+    mainHook.completeLessonComponent(csid, objScore); // sends the request to complete the lesson
     let csid2 = csid.replaceAll('_phx', '')
     let csid3 = csid2.replaceAll('.phx', '')
     if (csid3.includes('.20.v2_')) {
