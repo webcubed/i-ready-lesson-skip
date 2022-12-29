66d53527276f1f33c9cac97fd082e28b83de7b63
Nathan <77589042+oofish8888@users.noreply.github.com>
2022-12-28 20:35:59 -0500

    wang said 'yuck'

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ lessonBridge.pause() //calls the 'lessonBridge.pause' func.
 // part 2
 window.mainHook = _0xcc7fd3; // extracts the '_0xcc7fd3' variable to get the completeLessonComponent func.
 window.p1 = function(score) {
-     let csid = html5Iframe.src.split('csid=')[1].split('&type')[0] // gets the current lesson csid
+     window.csid = html5Iframe.src.split('csid=')[1].split('&type')[0] // gets the current lesson csid
     objScore = { score: score } //makes the score into an obj to make server happy
     mainHook.completeLessonComponent(csid, objScore); // sends the request to complete the lesson
-    let csid2 = csid.replaceAll('_phx', '')
-    let csid3 = csid2.replaceAll('.phx', '')
+    window.csid2 = csid.replaceAll('_phx', '')
+    window.csid3 = csid2.replaceAll('.phx', '')
     if (csid3.includes('.20.v2_')) {
         isQuiz= 'true';
     } else if (csid3.includes('.10.v2_')) {
