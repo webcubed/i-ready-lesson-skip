32281eae5045055316b5b5d4bbc79e36fc9d7bb3
oofish8888 <77589042+oofish8888@users.noreply.github.com>
2023-03-27 12:44:39 -0400

    no comments

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 //part 1
-debug(lessonBridge.pause) // exposes scope when function called
-lessonBridge.pause() //calls the 'lessonBridge.pause' func.
+debug(lessonBridge.pause)
+lessonBridge.pause()
 
 // part 2
-window.mainHook = _0x6c976a; // extracts the '_0xcc7fd3' variable to get the completeLessonComponent func.
+window.mainHook = _0x6c976a;
 window.p1 = function(score) {
-     window.csid = html5Iframe.src.split('csid=')[1].split('&type')[0] // gets the current lesson csid
-    objScore = { score: score } //makes the score into an obj to make server happy
-    mainHook.completeLessonComponent(csid, objScore); // sends the request to complete the lesson
+    window.csid = html5Iframe.src.split('csid=')[1].split('&type')[0]
+    objScore = { score: score }
+    mainHook.completeLessonComponent(csid, objScore);
     window.csid2 = csid.replaceAll('_phx', '')
     window.csid3 = csid2.replaceAll('.phx', '')
     window.csid5 = html5Iframe.src
