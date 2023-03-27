1adb84ebd8429769de5fcef767b3cb6e814c6517
oofish8888 <77589042+oofish8888@users.noreply.github.com>
2023-03-27 12:42:03 -0400

    we gotteem again

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ debug(lessonBridge.pause) // exposes scope when function called
 lessonBridge.pause() //calls the 'lessonBridge.pause' func.
 
 // part 2
-window.mainHook = _0xcc7fd3; // extracts the '_0xcc7fd3' variable to get the completeLessonComponent func.
+window.mainHook = _0x6c976a; // extracts the '_0xcc7fd3' variable to get the completeLessonComponent func.
 window.p1 = function(score) {
      window.csid = html5Iframe.src.split('csid=')[1].split('&type')[0] // gets the current lesson csid
     objScore = { score: score } //makes the score into an obj to make server happy
